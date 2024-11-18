ca1b5fa7b150e1a63e08c1fe789633b6dec3e59a
Kade Vaughan <kadevaughan6@gmail.com>
2024-11-17 18:10:11 -0700

    readme updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   root: './frontend', // Ensure this points to your frontend directory
   build: {
-    outDir: './frontend/dist', // Output directory for build
+    outDir: '../dist', // Output directory for build
     emptyOutDir: true,
   },
   plugins: [react()],
